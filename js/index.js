0d689e1f673de40a3473402ba24da6d531631849
NicoAita <aitanicolas@gmail.com>
2023-11-26 14:50:52 -0300

    ajustes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+
+
 // Variables 
 let precioTotal = 0;
 
@@ -5,11 +8,18 @@ let precioTotal = 0;
 
 const cardArticulo = document.querySelector(".contenedor-articulos");
 const buscar = document.querySelector("#buscador");
-const botonCarrito = document.querySelector(".contenedor-icono");
+const botonCarrito = document.querySelector(".contenedor-icono-carrito");
 const contenedorCarrito = document.querySelector(".contenedor-productos-carrito");
 const cerrarProductos = document.querySelectorAll(".cerrar-producto ");
 
-// Funciones para crear y cargar los productos
+//Llamé a estas funciones acá porque fue la fomra que encontré para que me cargue al carrito del ecommerce lo almacenado en el Local Storage.
+cargarProductos();
+mostrarCarrito();
+mostrarCantidadCarrito();
+
+
+// Funciones para crear las cards cargando los productos
+
 function crearCardError() {
    return `<div class="articulo-error">
    <figure>
@@ -24,19 +34,19 @@ function crearCardError() {
 function crearCardHTML(producto) {
    return `<div class="articulo">
       <div class= "imagen">
-          <img src="${producto.imagen}">
+         <img src="${producto.imagen}">
       </div>
       <div class="info-producto">
-          <div class="producto">${producto.nombre}</div>
-          <div class="precio">$ ${producto.precio}</div>
-          <button class="btn-agregar-carrito" id="${producto.codigo}">Añadir al carrito</button>
+         <div class="producto">${producto.nombre}</div>
+         <div class="precio">$ ${producto.precio}</div>
+         <button class="btn-agregar-carrito" id="${producto.codigo}">Añadir al carrito</button>
       </div>
-  </div>`
+</div>`
 };
 
 function cargarProductos() {
    if (productos.length > 0) {
-      cardArticulo.innerHTML = "";  
+      cardArticulo.innerHTML = ""; // Vacía el HTML desde JS
       productos.forEach((producto) => cardArticulo.innerHTML += crearCardHTML(producto));
       activarClickEnBotones();
    } else {
@@ -44,63 +54,61 @@ function cargarProductos() {
    }
 }
 
-// Lógica del carrito
-function agregarProductoAlCarrito(producto) {
-   const indiceProductoExistente = carritoUsuario.findIndex((prod) => prod.codigo === producto.codigo);
 
-   if (indiceProductoExistente !== -1) {
-      carritoUsuario[indiceProductoExistente].cantidad++;
-   } else {
-      carritoUsuario.push({ ...producto, cantidad: 1 });
-   }
+// LÓGICA CARRITO 
 
-   mostrarCarrito();
-   mostrarCantidadCarrito();
-}
-
-
-
+// Mostrar el carrito
 
 function mostrarCarrito() {
+
    contenedorCarrito.innerHTML = "";
- 
+
    carritoUsuario.forEach((producto) => {
       contenedorCarrito.innerHTML += `
+ 
+   <div class="fila-producto"> 
       <div class="info-producto-carrito"> 
-         <div class="producto-en-carrito">
-         <div class= "titulo-producto-carrito"> 
-            <div>${producto.nombre}</div>
-            <div>$ ${producto.precio}</div>
-            <div class= "cantidad-producto-carrito">Cantidad: ${producto.cantidad}</div>
-            <div class="cerrar-producto" data-id="${producto.codigo}">&#10006;</div>
-         </div>
-      </div>
-       `;
+      
+            <div class= "titulo-producto-carrito"> 
+               <div>${producto.nombre}</div>
+               <div>$ ${producto.precio}</div>
+               <div class= "cantidad-producto-carrito">Cantidad: ${producto.cantidad}</div>
+               <div data-id="${producto.codigo}"></div>
+            </div>
+
+      
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="icono-cerrar">
+
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+      </div>    
+   
+   </div> `;
+      console.log("Contenido del carrito:", carritoUsuario);
    });
 
    precioTotal = carritoUsuario.reduce((total, producto) => total + producto.precio * producto.cantidad, 0);
    contenedorCarrito.innerHTML += `
-       <div class="precio-producto-carrito">
-           <div>Total:</div>
-           <div>$ ${precioTotal}</div>
-       </div>
+      <div class="precio-producto-carrito total-carrito">
+         <div>Total:</div>
+         <div class="total-pagar"> $ ${precioTotal}</div>
+      </div>
    `;
-}
 
+};
 
-
-
-
+// Mostrar cantidad en el carrito
 
 function mostrarCantidadCarrito() {
    const contadorProducto = document.querySelector("#contador-productos");
+
    contadorProducto.textContent = carritoUsuario.reduce((total, producto) => total + producto.cantidad, 0).toString();
-}
+
+};
 
 
+// Sumar productos al Carrito
 
-
-// Función para sumar objetos al Array "carritoUsuario"
 function activarClickEnBotones() {
    const botonesAgregar = document.querySelectorAll(".btn-agregar-carrito");
    botonesAgregar.forEach((boton) => {
@@ -116,15 +124,16 @@ function activarClickEnBotones() {
             carritoUsuario.push(productoSeleccionado);
          }
 
+         guardarEnLocal();
          mostrarCarrito();
          mostrarCantidadCarrito();
+
       });
    });
-}
+};
 
 // BUSCADOR
 
-// EVENTO SEARCH PARA BUSCADOR y MÉTODOS
 buscar.addEventListener("search", () => {
    let productoBuscado = buscar.value.trim().toLowerCase();
 
@@ -133,20 +142,25 @@ buscar.addEventListener("search", () => {
    if (resultadosFiltrados.length === 0) {
       cardArticulo.innerHTML = crearCardError();
    } else {
-      cardArticulo.innerHTML = ""; // Vacía el contenedor de tarjetas antes de mostrar los nuevos resultados
+      cardArticulo.innerHTML = "";
 
       resultadosFiltrados.forEach((producto) => {
          cardArticulo.innerHTML += crearCardHTML(producto);
       });
+
    }
+
+   activarClickEnBotones()
+   guardarEnLocal();
+   mostrarCarrito();
+   mostrarCantidadCarrito();
 });
 
 
 
 
-// CARRITO ICONO
+// Ocultar y Mostrar el carrito tocando el ícono
 
-// EVENTO CLICK PARA OCULTAR Y ACTIVAR EL CARRITO DE COMPRAS
 botonCarrito.addEventListener("click", () => {
    if (contenedorCarrito.classList.contains("oculto-carrito")) {
       contenedorCarrito.classList.remove("oculto-carrito");
@@ -155,23 +169,29 @@ botonCarrito.addEventListener("click", () => {
    }
 });
 
+// Eliminar productos del carrito -> NO logro que funcione el evento sobre la cruz usando la clase "icono-cerrar". Probé varias cosas y no pude. El evento escucha el click, ya me fijé en consola. Pero no logro entender por qué no funciona la lógica. 
 
-
-
-// ELIMINAR PRODUCTOS DEL CARRITO
-contenedorCarrito.addEventListener('click', (e) => {
-   if (e.target.classList.contains('cerrar-producto')) {
+contenedorCarrito.addEventListener("click", (e) => {
+  /*  console.log("Elemento clickeado:", e.target); */
+   if (e.target.classList.contains("icono-cerrar")) {
       const id = parseInt(e.target.dataset.id);
       const indiceProducto = carritoUsuario.findIndex((producto) => producto.codigo === id);
       if (indiceProducto !== -1) {
          carritoUsuario.splice(indiceProducto, 1);
          mostrarCarrito();
-         mostrarCantidadCarrito(); // Actualizar contador
+         mostrarCantidadCarrito();
       }
    }
 });
 
+/* cargarProductos(); */
+
+//LOCAL Storage
+
+const guardarEnLocal = () => {
+   localStorage.setItem("carrito", JSON.stringify(carritoUsuario));
+};
 
 
 
-cargarProductos();
+
--- js/productos.js
+++ js/productos.js
@@ -1,13 +1,17 @@
 // PRODUCTOS
 
-const carritoUsuario = [];
+
+const carritoUsuario = JSON.parse(localStorage.getItem("carrito")) || []; //De esta forma le digo a JS que "carrito Usuario" es lo que está almacenado en el local storage, y si no tiene nada es que está vacío.
 
 const productos = [
-    
-{ codigo: 1, nombre: `Gorra HxH`, precio: 900, imagen:`./Imagenes/Gorra.png`},
-{ codigo: 2, nombre: `Remera HxH`, precio: 1000, imagen:`./Imagenes/Remera.png` },
-{ codigo: 3, nombre: `Mouse Pad HxH`, precio: 200, imagen:`./Imagenes/Mouse Pad.png` },
-{ codigo: 4, nombre: `Pantuflas HxH`, precio: 700, imagen:`./Imagenes/Pantuflas.png` },
-{ codigo: 5, nombre: `Taza HxH`, precio: 350, imagen:`./Imagenes/Taza.png` },
-{ codigo: 6, nombre: `Libreta HxH`, precio: 420, imagen:`./Imagenes/Libreta.png`}
+
+    { codigo: 1, nombre: `Gorra HxH`, precio: 900, imagen: `./Imagenes/Gorra.png` },
+    { codigo: 2, nombre: `Remera HxH`, precio: 1000, imagen: `./Imagenes/Remera.png` },
+    { codigo: 3, nombre: `Mouse Pad HxH`, precio: 200, imagen: `./Imagenes/Mouse Pad.png` },
+    { codigo: 4, nombre: `Pantuflas HxH`, precio: 700, imagen: `./Imagenes/Pantuflas.png` },
+    { codigo: 5, nombre: `Taza HxH`, precio: 350, imagen: `./Imagenes/Taza.png` },
+    { codigo: 6, nombre: `Libreta HxH`, precio: 420, imagen: `./Imagenes/Libreta.png` }
 ];
+
+
+
